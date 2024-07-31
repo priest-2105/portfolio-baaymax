afb251f1783f11b82eaf0132fb1833ad8755cb14
priest-2105 <fawzybailey782@gmail.com>
2024-07-31 20:55:10 +0100

    commit ✨✨

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,5 @@
 import { Container } from ".."
-import { Falinkedin, FaGithub, FaFacebook, FaVoicemail } from 'react-icons/fa'
+import { FaTwitter, FaGithub, FaFacebook, FaVoicemail } from 'react-icons/fa'
 import { AiFillMail } from "react-icons/ai"
 
 import { socials } from "../../data/socials.json"
@@ -21,7 +21,7 @@ function Footer() {
           </div>
           <div className="right">
             <div className="socials flex flex-row items-center justify-center">
-              {socials["linkedin"] !== "" && <SocialLink url={socials["linkedin"]} children={<Falinkedin />} />}
+              {socials["twitter"] !== "" && <SocialLink url={socials["twitter"]} children={<FaTwitter />} />}
 
               {socials["github"] !== "" && <SocialLink url={socials["github"]} children={<FaGithub />} />}
 
