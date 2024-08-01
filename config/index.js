c9f0cb8c116f4d18071579312321503cb566cb53
priest-2105 <fawzybailey782@gmail.com>
2024-08-01 12:14:34 +0100

    ✨✨ commit

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@
 
 // EMAILJS Config Data
 
-export const EMAILJS_TEMPLATE_ID = "template_kh9buql"
+export const EMAILJS_TEMPLATE_ID = "template_xml2wjr"
 
-export const EMAILJS_SERVICE_ID = "service_hd5qbz2"
+export const EMAILJS_SERVICE_ID = "service_xghtk94"
 
-export const EMAILJS_PUBLIC_KEY = "user_SXwbm2bfwtluGBhARcTaf"
+export const EMAILJS_PUBLIC_KEY = "oDEEeCfTBH-_mPEw4"
